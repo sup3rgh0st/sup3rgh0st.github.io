33c51f828190a70b88078ee6439e688f095d3ead
sup3rgh0st <Ryan.Magliola@gmail.com>
2016-01-30 01:50:28 -0500

    WiiUSite
    WiiUSite

---
+++ WiiUSite/apps/helloworld/wiiuhaxx_common_cfg.php
@@ -0,0 +1,6 @@
+<?php
+
+$wiiuhaxxcfg_payloadfilepath = "./helloworld550.bin";
+$wiiuhaxxcfg_loaderfilepath = "./wiiuhaxx_loader.bin"; //<-- Will redirect to the loader on my server, you can change it also to your own.
+
+?>
